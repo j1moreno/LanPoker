5335baad040c9cc4471bfda7457c091ee619a06e
j1moreno <jmoreno.0312@gmail.com>
2019-11-10 21:45:42 -0800

    Initial commit for work done so far

---
+++ server/routes/CardDeck.js
@@ -0,0 +1,61 @@
+const DECK_SIZE = 52;
+
+const indexToRank = [
+    "Ace",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "Jack",
+    "Queen",
+    "King"
+]
+
+const indexToSuit = [
+    "Spades",
+    "Clubs",
+    "Diamonds",
+    "Hearts"
+]
+
+class CardDeck {
+    constructor() {
+        this.numbers = Array.from(Array(DECK_SIZE).keys());
+    }
+
+    drawCard() {
+        do {
+            if (this.numbers.length < 1) return {"rank": -1, "suit": "none"}
+            var randomIndex = Math.floor(Math.random() * DECK_SIZE);
+        } while (!this.numbers.includes(randomIndex))
+        // if we got here, a new card index was selected;
+        // remove it from array so we don't get it again
+        var index = this.numbers.indexOf(randomIndex);
+        if (index > -1) {
+        this.numbers.splice(index, 1);
+        }
+
+        return {
+            "index": randomIndex,
+            "rank": indexToRank[randomIndex % 13],
+            "suit": indexToSuit[randomIndex % 4]
+        };
+    }
+
+    drawCards(numberOfCards) {
+        let cards = []
+        for (let i = 0; i < numberOfCards; i++) {
+            cards.push(this.drawCard())
+        }
+
+        return cards
+    }
+
+}
+
+module.exports = CardDeck;
