21375341716232b22596d918dc1731cc385fba4e
j1moreno <jmoreno.0312@gmail.com>
2019-12-15 13:28:33 -0800

    allow card flipping; card back is joker for now

--- client/src/Card.js
+++ client/src/Card.js
@@ -1,18 +1,26 @@
 import React from "react";
 
 const Card = props => {
-  return (
-    <img
-      alt=""
-      src={
-        "/images/SVG-cards-1.3/" +
-        props.cardInfo.rank +
-        "_of_" +
-        props.cardInfo.suit +
-        ".svg"
-      }
-    />
-  );
+  const displayCard = () => {
+    if (props.faceUp) {
+      return (
+        <img
+          alt=""
+          src={
+            "/images/SVG-cards-1.3/" +
+            props.cardInfo.rank +
+            "_of_" +
+            props.cardInfo.suit +
+            ".svg"
+          }
+        />
+      );
+    } else {
+      return <img alt="" src={"/images/SVG-cards-1.3/black_joker.svg"} />;
+    }
+  };
+
+  return <div>{displayCard()}</div>;
 };
 
 export default Card;
