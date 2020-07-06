9900202045427b5b5abed33aa3e175ed626897dc
j1moreno <jmoreno.0312@gmail.com>
2020-07-06 02:09:23 -0700

    match casing for card file names; minor formatting

--- server/routes/CardDeck.js
+++ server/routes/CardDeck.js
@@ -1,61 +1,55 @@
 const DECK_SIZE = 52;
 
 const indexToRank = [
-    "Ace",
-    "2",
-    "3",
-    "4",
-    "5",
-    "6",
-    "7",
-    "8",
-    "9",
-    "10",
-    "Jack",
-    "Queen",
-    "King"
-]
+  "Ace",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "jack",
+  "queen",
+  "king",
+];
 
-const indexToSuit = [
-    "Spades",
-    "Clubs",
-    "Diamonds",
-    "Hearts"
-]
+const indexToSuit = ["spades", "clubs", "diamonds", "hearts"];
 
 class CardDeck {
-    constructor() {
-        this.numbers = Array.from(Array(DECK_SIZE).keys());
+  constructor() {
+    this.numbers = Array.from(Array(DECK_SIZE).keys());
+  }
+
+  drawCard() {
+    do {
+      if (this.numbers.length < 1) return { rank: -1, suit: "none" };
+      var randomIndex = Math.floor(Math.random() * DECK_SIZE);
+    } while (!this.numbers.includes(randomIndex));
+    // if we got here, a new card index was selected;
+    // remove it from array so we don't get it again
+    var index = this.numbers.indexOf(randomIndex);
+    if (index > -1) {
+      this.numbers.splice(index, 1);
     }
 
-    drawCard() {
-        do {
-            if (this.numbers.length < 1) return {"rank": -1, "suit": "none"}
-            var randomIndex = Math.floor(Math.random() * DECK_SIZE);
-        } while (!this.numbers.includes(randomIndex))
-        // if we got here, a new card index was selected;
-        // remove it from array so we don't get it again
-        var index = this.numbers.indexOf(randomIndex);
-        if (index > -1) {
-        this.numbers.splice(index, 1);
-        }
+    return {
+      index: randomIndex,
+      rank: indexToRank[randomIndex % 13],
+      suit: indexToSuit[randomIndex % 4],
+    };
+  }
 
-        return {
-            "index": randomIndex,
-            "rank": indexToRank[randomIndex % 13],
-            "suit": indexToSuit[randomIndex % 4]
-        };
+  drawCards(numberOfCards) {
+    let cards = [];
+    for (let i = 0; i < numberOfCards; i++) {
+      cards.push(this.drawCard());
     }
 
-    drawCards(numberOfCards) {
-        let cards = []
-        for (let i = 0; i < numberOfCards; i++) {
-            cards.push(this.drawCard())
-        }
-
-        return cards
-    }
-
+    return cards;
+  }
 }
 
 module.exports = CardDeck;
